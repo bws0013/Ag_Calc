fac4d290842060a9e8153e95ea4a3c8176fbb3c9
Ben <i@auburn.edu>
2018-03-22 17:41:24 -0500

    More data validation

--- code/read.py
+++ code/read.py
@@ -8,19 +8,23 @@ def getstuff(filename, criterion):
     with open(filename, "rb") as csvfile:
         datareader = csv.reader(csvfile)
         count = 0
+        m = 0
         for row in datareader:
+            m += 1
             # count += 1
             # print row[3]
             # yield row
+            # if m > 1000:
             if row[3] in ("column header", criterion):
-                # yield row
+                yield row
                 count += 1
-            # if count > 10:
-            #     return
-        return count
+            if count > 5:
+                return
+        # return count
 
 ff = getstuff("./Data/FeedGrains.csv", "Barley")
-print ff
 # print ff
-# for i in ff:
-#     print(i)
+print ff
+for i in ff:
+    print(i)
+    print(i[15])
