ad3216b4bbb2f1de5a07602abfb91f3251c364bd
Ben <i@auburn.edu>
2018-03-23 18:28:09 -0500

    I have to figure out how pricce is working here, inflation and that

--- code/read.py
+++ code/read.py
@@ -1,5 +1,6 @@
 import csv
-
+import sys
+from sets import Set
 # Using these 2 to effectively read the large data file
 # https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
 # https://stackoverflow.com/questions/7883962/where-to-use-yield-in-python-best
@@ -15,16 +16,26 @@ def getstuff(filename, criterion):
             # print row[3]
             # yield row
             # if m > 1000:
-            if row[3] in ("column header", criterion):
+            if row[3] in ("column header", criterion) and len(row[17]) == 3:
                 yield row
                 count += 1
-            if count > 5:
-                return
+                print row
+                sys.exit(0)
+            # if count > 5:
+            #     return
         # return count
 
 ff = getstuff("./Data/FeedGrains.csv", "Barley")
 # print ff
-print ff
+# print ff
+months = Set()
+
 for i in ff:
-    print(i)
-    print(i[15])
+    # print(i)
+    months.add(i[17])
+    # print(i[17])
+
+
+
+for m in months:
+    print m
